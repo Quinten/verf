632a83df026f310dee8e84f19700b5d5806a8d34
Quinten Clause <quinten.clause@studioemma.eu>
2020-05-12 21:02:00 +0200

    Add all classes to export

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 export {default as VerfGame} from './src/VerfGame.js';
+export {default as Engine} from './src/core/Engine.js';
 export {default as Scene} from './src/core/Scene.js';
+export {default as Camera} from './src/core/Camera.js';
 export {default as Events} from './src/core/Events.js';
 export {default as GameObject} from './src/gameobjects/GameObject.js';
