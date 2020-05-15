6f48e9d840f80c8c87a1997612fce211942234ed
Quinten Clause <quinten.clause@studioemma.eu>
2020-05-15 22:43:26 +0200

    Remove some old code

--- src/gameobjects/SimpleBar.js
+++ src/gameobjects/SimpleBar.js
@@ -14,10 +14,6 @@ class SimpleBar extends GameObject {
 
     draw (context)
     {
-        //progress.lineStyle(2, window.fgColor.color, 1);
-        //progress.strokeRect(Math.floor((this.scale.width / 2) - 132), Math.floor((this.scale.height / 2) - 20), 264, 40);
-        //progress.fillStyle(window.fgColor.color, 1);
-        //progress.fillRect(Math.floor((this.scale.width / 2) - 128), Math.floor((this.scale.height / 2) - 16), 256 * value, 32);
         context.strokeStyle = this.fillStyle;
         context.lineWidth = 2;
         context.strokeRect(-this.width / 2, -this.height / 2, this.width, this.height);
