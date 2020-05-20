0cb26022e288f46437084d67750f650decab38d2
Quinten Clause <quinten.clause@studioemma.eu>
2020-05-21 00:30:38 +0200

    Add collision between 2 dynamic bodies

--- src/physics/World.js
+++ src/physics/World.js
@@ -155,6 +155,96 @@ class World {
         }
     }
 
+    seperateBoth(a, b)
+    {
+        // To find the side of entry calculate based on
+        // the normalized sides
+        let dx = (b.midX - a.midX) / b.halfWidth;
+        let dy = (b.midY - a.midY) / b.halfHeight;
+
+        // Calculate the absolute change in x and y
+        let absDX = Math.abs(dx);
+        let absDY = Math.abs(dy);
+
+        // If the distance between the normalized x and y
+        // position is less than a small threshold (.1 in this case)
+        // then this object is approaching from a corner
+        if (Math.abs(absDX - absDY) < .1) {
+
+            // If a is approaching from the right
+            if (dx < 0) {
+                let overlap = (b.right - a.left) / 2;
+                a.x += overlap;
+                b.x -= overlap;
+
+            // If a is approaching from the left
+            } else {
+                let overlap = (a.right - b.left) / 2;
+                a.x -= overlap;
+                b.x += overlap;
+            }
+
+            // If a is approaching from the bottom
+            if (dy < 0) {
+                let overlap = (b.bottom - a.top) / 2;
+                a.y += overlap;
+                b.y -= overlap;
+
+            // If a is approaching from the top
+            } else {
+                let overlap = (a.bottom - b.top) / 2;
+                a.y -= overlap;
+                b.y += overlap;
+            }
+
+            // Randomly select a x/y direction to reflect velocity on
+            if (Math.random() < .5) {
+                a.vx = -a.vx * b.restitution;
+                b.vx = -b.vx * a.restitution;
+            } else {
+                a.vy = -a.vy * b.restitution;
+                b.vy = -b.vy * a.restitution;
+            }
+
+        // If a is approaching from the sides
+        } else if (absDX > absDY) {
+
+            // If a is approaching from the right
+            if (dx < 0) {
+                let overlap = (b.right - a.left) / 2;
+                a.x += overlap;
+                b.x -= overlap;
+            } else {
+            // If a is approaching from the left
+                let overlap = (a.right - b.left) / 2;
+                a.x -= overlap;
+                b.x += overlap;
+            }
+
+            a.vx = -a.vx * b.restitution;
+            b.vx = -b.vx * a.restitution;
+
+        // If this collision is coming from the top or bottom
+        } else {
+
+            // If a is approaching from the bottom
+            if (dy < 0) {
+                let overlap = (b.bottom - a.top) / 2;
+                a.y += overlap;
+                b.y -= overlap;
+
+            } else {
+            // If a is approaching from the top
+                let overlap = (a.bottom - b.top) / 2;
+                a.y -= overlap;
+                b.y += overlap;
+            }
+
+            a.vy = -a.vy * b.restitution;
+            b.vy = -b.vy * a.restitution;
+        }
+    }
+
     step(delta)
     {
         delta = delta / 1000; // convert delta to seconds, so we can define velocity in pixels per second
@@ -184,7 +274,7 @@ class World {
                 } else if (collider.a.immovable && !collider.b.immovable) {
                     this.seperateAFromB(collider.b, collider.a);
                 } else if (!collider.a.immovable && !collider.b.immovable) {
-                    // TODO: create a seperateBoth function
+                    this.seperateBoth(collider.a, collider.b);
                 }
                 if (collider.callback) {
                     collider.callback(collider.a, collider.b);
